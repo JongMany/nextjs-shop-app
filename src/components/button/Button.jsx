0ffbba7222f741ecf28228e9b72f6b3c38ec09a5
JongMany <blackberry1114@naver.com>
2024-08-27 23:39:44 +0900

    feat: Button 컴포넌트 구현

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
-function Button() {
-  return <div>Button</div>;
+function Button({
+  type = "button",
+  secondary = false,
+  bgColor,
+  fgColor,
+  width,
+  ...restProps
+}) {
+  const composeClasses = classNames(
+    styles.button,
+    secondary ? styles.secondary : styles.primary
+  );
+
+  const style = {
+    backgroundColor: bgColor || "",
+    color: fgColor || "",
+    width: width || "",
+  };
+
+  return (
+    <button
+      className={composeClasses}
+      type={type}
+      style={style}
+      {...restProps}
+    />
+  );
 }
 
 export default Button;
