547e393197565678f8d04e263847a05151089723
JongMany <blackberry1114@naver.com>
2024-08-28 08:11:55 +0900

    feat: 회원가입 기능 추가

--- src/app/(auth)/register/RegisterClient.jsx
+++ src/app/(auth)/register/RegisterClient.jsx
@@ -14,6 +14,7 @@ import Loader from "@/components/loader/Loader";
 import Image from "next/image";
 
 import styles from "../login/Auth.module.scss";
+import { auth } from "@/firebase/firebase";
 
 function RegisterClient() {
   const [email, setEmail] = useState("");
